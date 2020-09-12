66d50ce47d0de6bfd37d58709afcc1e45f8f8dfd
Chirag Kushwaha <chiragkushwaha1811@gmail.com>
2020-09-12 12:51:10 +0530

    website ui

---
+++ src/firebase.js
@@ -0,0 +1,11 @@
+const firebaseConfig = {
+  apiKey: 'AIzaSyCkPMDDj_IBKGGFbQ3FazkPc1bONd8I_OU',
+  authDomain: 'netflixi.firebaseapp.com',
+  databaseURL: 'https://netflixi.firebaseio.com',
+  projectId: 'netflixi',
+  storageBucket: 'netflixi.appspot.com',
+  messagingSenderId: '81226667162',
+  appId: '1:81226667162:web:cb3b1a6c0f62f7350f7b23',
+  measurementId: 'G-QPV0L8K5PD',
+};
+
